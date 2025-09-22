fbb3e0530805879d04f9eeaf0f2f45d4b838a605
Bryan Agan <bryanagan25@gmail.com>
2025-09-23 07:44:32 +0800

    feat(frontend): create login page

---
+++ frontend/app/pages/login.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui';
+
+definePageMeta({
+  layout: 'auth',
+});
+
+const state = reactive({
+  email: '',
+  password: '',
+});
+
+const onSubmit = (event: FormSubmitEvent<typeof state>) => {
+  console.log(event.data);
+};
+</script>
+
+<template>
+  <div class="flex flex-col bg-zinc-800 w-96 h-96 rounded-xl py-5 gap-4">
+    <!-- Header -->
+    <div class="flex justify-center items-center gap-2 text-primary">
+      <Icon name="simple-icons:progress" class="w-7 h-7" />
+      <h3 class="font-bold text-xl">Sequence</h3>
+    </div>
+
+    <!-- Text -->
+    <div class="flex flex-col justify-center items-center gap-1">
+      <h2 class="font-bold text-2xl">Login with your credentials</h2>
+      <p class="text-base">Glad to have you back!</p>
+    </div>
+
+    <!-- Form -->
+    <UForm :state="state" class="flex flex-col gap-4 px-6" @submit="onSubmit">
+      <UFormField label="Email" name="email">
+        <UInput v-model="state.email" class="w-full" icon="tabler:mail" />
+      </UFormField>
+
+      <UFormField label="Password" name="password">
+        <UInput v-model="state.password" type="password" class="w-full" icon="tabler:lock" />
+      </UFormField>
+
+      <UButton type="submit" class="justify-center cursor-pointer bg-primary-500">Sign in</UButton>
+    </UForm>
+
+    <div class="flex justify-center items-center gap-2">
+      <p class="text-base">Don't have an account?</p>
+      <p class="text-base font-bold text-primary cursor-pointer">Sign Up</p>
+    </div>
+  </div>
+</template>
