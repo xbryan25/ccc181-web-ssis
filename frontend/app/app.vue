88b7fe68405d2453805611f52df44d41778e2d04
Bryan Agan <bryanagan25@gmail.com>
2025-10-14 09:23:04 +0800

    feat(frontend): change nuxt rendering from SSR to SPA

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+
 const toaster = { position: 'top-right' } as const;
+
+onMounted(() => {
+  window.dispatchEvent(new Event('nuxt-ready'));
+});
 </script>
 
 <template>
