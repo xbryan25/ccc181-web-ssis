a59228954f4526503928b56cd348515e89d67c2c
Bryan Agan <bryanagan25@gmail.com>
2025-10-17 23:41:52 +0800

    fix(backend): fix search not working properly for gender and year_level because the are enum

--- app/db/queries/common.py
+++ app/db/queries/common.py
@@ -9,7 +9,7 @@ class CommonQueries:
     GET_TOTAL_COUNT_WITH_SEARCH = "SELECT COUNT(*) FROM {table} WHERE {search_by} ILIKE %s"
     GET_BY_ID = "SELECT * FROM {table} WHERE {pk} = %s LIMIT 1"
     GET_ALL = "SELECT * FROM {table}"
-    GET_MANY = "SELECT * FROM {table} WHERE {search_by} ILIKE %s ORDER BY {sort_field} {sort_order} LIMIT %s OFFSET %s"
+    GET_MANY = "SELECT * FROM {table} WHERE {search_by}::text ILIKE %s ORDER BY {sort_field} {sort_order} LIMIT %s OFFSET %s"
     UPDATE_BY_ID = "UPDATE {table} SET {set_clause} WHERE {pk} = %s"
     DELETE_BY_ID = "DELETE FROM {table} WHERE {pk} = %s"
     GET_ALL_IDS = "SELECT {columns} FROM {table} ORDER BY {order_column} ASC"
